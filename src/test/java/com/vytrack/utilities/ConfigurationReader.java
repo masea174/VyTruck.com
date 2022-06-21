79c73cbe0e8574573a3d977241d0483910db2964
Ryan1 <ryan15227@gmail.com>
2022-06-21 13:44:40 -0400

    Created configuration property file and Configuration reader class

---
+++ src/test/java/com/vytrack/utilities/ConfigurationReader.java
@@ -0,0 +1,28 @@
+package com.vytrack.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private ConfigurationReader(){}
+
+    private static Properties properties = new Properties();
+
+// :D
+    static {
+        try {
+            FileInputStream file = new FileInputStream("configuration.properties");
+            properties.load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+    }
+
+
+}
