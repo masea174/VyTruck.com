dacc6413e05cc2278c5e623ce42a6ce2c7112bda
Ryan1 <ryan15227@gmail.com>
2022-06-21 14:14:03 -0400

    Driver class has been made

---
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -0,0 +1,50 @@
+package com.vytrack.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver(){}
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        if(driver == null){
+            String browser = ConfigurationReader.getProperty("browser");
+            switch (browser) {
+                case "chrome":
+                default:
+
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+                case "firefox":
+
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+            }
+        }
+        return driver;
+    }
+
+    public static void driverClose(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+
+
+    }
+
+}
